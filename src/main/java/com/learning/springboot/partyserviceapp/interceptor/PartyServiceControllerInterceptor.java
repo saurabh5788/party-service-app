17315be7a169a1f887a473225d1a8090f6e69cd6
saurabh5788 <saurabh.singh5788@gmail.com>
2019-06-15 18:05:17 +0530

    Enabling Spring Auto FReconfiguration For DB Connectivity in CLoud Foundry

--- src/main/java/com/learning/springboot/partyserviceapp/interceptor/PartyServiceControllerInterceptor.java
+++ src/main/java/com/learning/springboot/partyserviceapp/interceptor/PartyServiceControllerInterceptor.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,11 +14,22 @@ import org.springframework.web.servlet.ModelAndView;
 public class PartyServiceControllerInterceptor implements HandlerInterceptor {
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(PartyServiceControllerInterceptor.class);
-
+	@Value("${spring.datasource.url:NO DS URL}")
+	private String dataSourceURL;
+	@Value("${spring.datasource.username:NO DS USERNAME}")
+	private String dataSourceUserName;
+	@Value("${vcap.services.party-service-app-db.name:NO VCAP DB NAME}")
+	private String dataSourceDBName;
+	@Value("${spring.datasource.password:NO PASSWORD}")
+	private String dataSourcePassword;
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
 		LOGGER.info("'preHandle' Method Calling!!!");
+		LOGGER.info("Data Source URL : {}", dataSourceURL);
+		LOGGER.info("Data Source DB Name : {}", dataSourceDBName);
+		LOGGER.info("Data Source UserName : {}", dataSourceUserName);
+		LOGGER.info("Data Source Password : {}", dataSourcePassword);
 		return true;
 	}
 
